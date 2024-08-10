292a2805b323c32bcaec501dbd174df79bcc0905
Huseyin Soylu <huseyinsoylu@protonmail.com>
2024-08-10 16:00:24 +0300

    feat(backend): cors added and code updated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,13 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import postRoutes from "./routes/posts";
 import createTableRoute from "./routes/createTableRoute";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 app.use(bodyParser.json());
+app.use(cors());
 app.use("/api/posts", postRoutes);
 app.use("/create-table", createTableRoute);
 
